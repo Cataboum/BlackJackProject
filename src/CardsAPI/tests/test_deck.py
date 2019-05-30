595a9123d9292c0420e8aa383c87a69d462d075c
ldehaine <louyann.dehaine@gmail.com>
2019-05-30 18:51:24 +0200

    Update Deck shuffling and related tests

--- src/CardsAPI/tests/test_deck.py
+++ src/CardsAPI/tests/test_deck.py
@@ -1,21 +1,22 @@
 from src.CardsAPI import Deck,Card
 
 
-def testInitDeck():
+def test_init_deck():
 
     deck = Deck.Deck()
     assert deck.top_card_index == 0
     assert len(deck.cards) == 312
-    assert deck.red_card_index >= ((3 / 4) * 312) - 30 and deck.red_card_index <= ((3 / 4) * 312) + 30
+    # Check placement of the red Card
+    assert ((3 / 4) * 312) - 30 <= deck.red_card_index <= ((3 / 4) * 312) + 30
 
     # Assert not shuffle
     deck = Deck.Deck(False)
     assert deck.top_card_index == 0
     assert len(deck.cards) == 312
-    assert deck.red_card_index >= ((3 / 4) * 312) - 30 and deck.red_card_index <= ((3 / 4) * 312) + 30
+    assert ((3 / 4) * 312) - 30 <= deck.red_card_index <= ((3 / 4) * 312) + 30
 
 
-def testDealDeck():
+def test_deal_deck():
 
     deck = Deck.Deck()
     card = deck.getCard()
@@ -23,7 +24,7 @@ def testDealDeck():
     assert deck.top_card_index == 1
 
 
-def testRedCardDeck():
+def test_red_card_deck():
 
     deck = Deck.Deck()
     last_card = deck.cards[-1]
@@ -36,25 +37,42 @@ def testRedCardDeck():
     assert deck.cards[-1] != last_card
 
 
-def testShuffleDeck():
+def test_shuffle_deck():
 
     deck = Deck.Deck()
-    first_card = deck.cards[0]
+    try:
+        for i in range(500):
+            card = deck.getCard()
+    except IndexError:
+        assert True
+    else:
+        # This shouldn't work because the Deck is only 312 cards long
+        assert False
+
+    deck = Deck.Deck()
+
+    for i in range(deck.red_card_index):
+        assert not deck.needs_shuffling
+        deck.getCard()
+
+    assert deck.needs_shuffling
+
     deck.shuffle()
-    assert first_card != deck.cards[0]
+
+    assert not deck.needs_shuffling
+
+    try:
+        # This won't work if the deck has not been shuffled
+        for i in range(
+                (len(deck.cards) - deck.red_card_index) + 1
+        ):
+            deck.getCard()
+    except IndexError:
+        assert False
 
 
-def testInitShuffleDeck():
+def test_init_shuffle_deck():
 
     deck1 = Deck.Deck(False)
     deck2 = Deck.Deck(False)
     assert deck1.cards[0] == deck2.cards[0]
-
-
-def testToMuchDeal():
-
-    deck = Deck.Deck()
-    for i in range(500):
-        card = deck.getCard()
-
-    assert len(deck.cards) == 312
